a0076e4cd3e94ec42f5bff75bd866665b4854e71
ifenty <ifenty@gmail.com>
2022-12-09 15:02:44 -0800

    first commit of ian dev branch of ecco_pipeline

--- ecco_pipeline/conf/global_settings.py
+++ ecco_pipeline/conf/global_settings.py
@@ -3,10 +3,11 @@ from pathlib import Path
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 #OUTPUT_DIR = Path('/Users/marlis/Developer/ECCO/ecco_output')
-OUTPUT_DIR = Path('/ecco_nfs_1/shared/ECCO-pipeline/observations')
+#OUTPUT_DIR = Path('/ecco_nfs_1/shared/ECCO-pipeline/observations')
+OUTPUT_DIR = Path('/ecco_nfs_1/shared/ECCO_pipeline_dev_ian/observations')
 GRIDS = ['ECCO_llc90']
 
 SOLR_HOST = 'http://localhost:8983/solr/'
-SOLR_COLLECTION = 'ecco_datasets'
+SOLR_COLLECTION = 'ecco_dev_ian'
 
 os.chdir(ROOT_DIR)
