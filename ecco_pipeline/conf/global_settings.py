68366c9d60d86eba1b34472840ddd50f396014ce
ifenty <ifenty@gmail.com>
2022-12-09 14:53:54 -0800

    setting up pipeline to work on cnidarian

--- ecco_pipeline/conf/global_settings.py
+++ ecco_pipeline/conf/global_settings.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-OUTPUT_DIR = Path('/Users/marlis/Developer/ECCO/ecco_output')
-
+#OUTPUT_DIR = Path('/Users/marlis/Developer/ECCO/ecco_output')
+OUTPUT_DIR = Path('/ecco_nfs_1/shared/ECCO-pipeline/observations')
 GRIDS = ['ECCO_llc90']
 
 SOLR_HOST = 'http://localhost:8983/solr/'
