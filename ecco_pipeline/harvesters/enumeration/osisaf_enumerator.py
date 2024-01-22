c517281813989eba3371fe99270e35d70d02d042
kevinmarlis <kevinmarlis@gmail.com>
2024-01-22 10:39:07 -0800

    Bug fixes

--- ecco_pipeline/harvesters/enumeration/osisaf_enumerator.py
+++ ecco_pipeline/harvesters/enumeration/osisaf_enumerator.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 from harvesters.harvester import Harvester
 
-OSISAF_URL = 'https://thredds.met.no/thredds/catalog/osisaf/met.no/reprocessed/ice/'
+OSISAF_URL = 'https://thredds.met.no/thredds/catalog/osisaf/met.no/'
     
 def search_osisaf(harvester: Harvester):
     harvester.data_time_scale
@@ -20,29 +20,29 @@ def search_osisaf(harvester: Harvester):
         base_url = os.path.join(OSISAF_URL, harvester.ddir)
 
     session = requests.session()
-    r = session.get(os.path.join(base_url, 'catalog.xml'))
+    r = session.get(os.path.join(base_url, 'catalog.xml'))    
     data = BeautifulSoup(r.text, 'xml')
+    
     all_granules = []
-    for year_dir in data.find_all('catalogref'):
+    for year_dir in data.find_all('catalogRef'):
         if 'monthly' in year_dir['xlink:title']:
             continue
         r = session.get(os.path.join(base_url, year_dir['xlink:title'], 'catalog.xml'))
         year_data = BeautifulSoup(r.text, 'xml')
-
         if harvester.data_time_scale == 'daily':
-            for month_dir in year_data.find_all('catalogref'):
+            for month_dir in year_data.find_all('catalogRef'):
                 month_url = os.path.join(base_url, year_dir['xlink:title'], month_dir['xlink:title'], 'catalog.xml')
                 r = session.get(month_url)
                 month_data = BeautifulSoup(r.text, 'xml')
                 for dataset in month_data.find_all('dataset'):
                     for granule in dataset.find_all('dataset'):
-                        url = os.path.join('https://thredds.met.no/thredds/fileServer/', granule['urlpath'])
+                        url = os.path.join('https://thredds.met.no/thredds/fileServer/', granule['urlPath'])
                         mod_time = datetime.strptime(granule.find('date').text, '%Y-%m-%dT%H:%M:%SZ')
                         all_granules.append(OSISAFGranule(url, mod_time))
         else:
             for dataset in year_data.find_all('dataset'):
                 for granule in dataset.find_all('dataset'):
-                    url = os.path.join('https://thredds.met.no/thredds/fileServer/', granule['urlpath'])
+                    url = os.path.join('https://thredds.met.no/thredds/fileServer/', granule['urlPath'])
                     mod_time = datetime.strptime(granule.find('date').text, '%Y-%m-%dT%H:%M:%SZ')
                     all_granules.append(OSISAFGranule(url, mod_time))
     logging.info(f'Found {len(all_granules)} possible granules')
