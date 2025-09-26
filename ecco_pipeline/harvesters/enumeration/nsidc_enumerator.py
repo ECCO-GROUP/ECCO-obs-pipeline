c429fea8d27f709f1ae9c45c0ae8eb4aba9a2cd7
Kevin <kevinmarlis@gmail.com>
2025-09-26 14:54:57 -0700

    G10016 v3 (#86)
    * Added support for optional ddir config parameter
    
    * Generalized the nsidc seaice functions to be used across both datasets
    
    * Bug fix for missing self argument
    
    * Updated function names to use generalized versions
    
    * Deprecated G10016 V2 in favor of V3

--- ecco_pipeline/harvesters/enumeration/nsidc_enumerator.py
+++ ecco_pipeline/harvesters/enumeration/nsidc_enumerator.py
@@ -18,7 +18,10 @@ def search_nsidc(harvester: Harvester):
     date_range = range(harvester.start.year, harvester.end.year + 1)
     all_granules = []
     for hemi in ["north", "south"]:
-        ds_hemi_url = os.path.join(NSIDC_URL, harvester.ds_name, hemi, "daily")
+        if harvester.ddir:
+            ds_hemi_url = os.path.join(NSIDC_URL, harvester.ds_name, harvester.ddir, hemi, "daily")
+        else:
+            ds_hemi_url = os.path.join(NSIDC_URL, harvester.ds_name, hemi, "daily")
         r = requests.get(ds_hemi_url)
         data = BeautifulSoup(r.text, "html.parser")
         years = [link["href"].replace("/", "") for link in data.find_all("a")[1:]]
@@ -31,9 +34,7 @@ def search_nsidc(harvester: Harvester):
             for link in data.find_all("a")[1:]:
                 url = os.path.join(ds_hemi_year_url, link["href"])
                 tokens = link.next_sibling.split()
-                mod_time = datetime.strptime(
-                    tokens[0] + " " + tokens[1], "%d-%b-%Y %H:%M"
-                )
+                mod_time = datetime.strptime(tokens[0] + " " + tokens[1], "%d-%b-%Y %H:%M")
                 all_granules.append(NSIDCGranule(url, mod_time))
     logger.info(f"Found {len(all_granules)} possible granules. Checking for updates...")
     return all_granules
